d8a37bf5cb8a5984851a36a1d01d9958e49f14e6
simondotm <simondotm@users.noreply.github.com>
2019-01-29 21:53:12 +0000

    Refactored for added huffman

--- vgmplayer.asm
+++ vgmplayer.asm
@@ -5,6 +5,7 @@
 ; VGM player uses 11 zero page vars from this address
 VGM_ZP = &80
 LZ4_FORMAT = FALSE
+USE_HUFFMAN = FALSE
 
 ; Allocate vars in ZP
 .zp_start
@@ -85,26 +86,37 @@ ALIGN 256
 ; VGM library code
 ;---------------------------------------------------------------
 
+.decoder_start
+
+
 ;-------------------------------
 ; lz4 decoder
 ;-------------------------------
-.lz_start
 
-; 8 zero page registers used
+; declare zero page registers used for each compressed stream (they are context switched)
 lz_zp = VGM_ZP + 0
-zp_stream_src   = lz_zp + 0    ; stream data ptr LO/HI
+zp_stream_src   = lz_zp + 0    ; stream data ptr LO/HI          *** ONLY USED ONCE PER FRAME ***, not worth ZP?
 zp_literal_cnt  = lz_zp + 2    ; literal count LO/HI
 zp_match_cnt    = lz_zp + 4    ; match count LO/HI
 zp_window_src   = lz_zp + 6    ; window read ptr - index
 zp_window_dst   = lz_zp + 7    ; window write ptr - index
-; 8 bytes total workspace
 
-; these variables are not preserved across context switches
-zp_buffer = lz_zp + 8          ; Lo byte of current decode window buffer
-zp_temp = lz_zp + 10           ; 2 bytes
-zp_stash = lz_zp + 12          ; 1 byte
-; 13 bytes total
+IF USE_HUFFMAN
+huff_bitbuffer  = lz_zp + 8    ; HUFF_ZP + 0   ; 1 byte
+huff_bitsleft   = lz_zp + 9    ; HUFF_ZP + 1   ; 1 byte
+lz_zp_size = 16  ; number of bytes total workspace for a stream
+ELSE
+lz_zp_size = 8  ; number of bytes total workspace for a stream
+ENDIF
 
+; these variables are not preserved across context switches, can be any Zero page
+zp_buffer = lz_zp + 10          ; 2 bytes, current decode window buffer
+zp_temp = lz_zp + 12           ; 2 bytes
+zp_stash = lz_zp + 14          ; 1 byte
+
+
+; If Huffman is enabled, lz_fetch_byte is declared below.
+IF USE_HUFFMAN == FALSE
 ; fetch a byte from the currently selected compressed register data stream
 ; returns byte in A, clobbers Y
 .lz_fetch_byte
@@ -117,6 +129,7 @@ zp_stash = lz_zp + 12          ; 1 byte
 .ok
     rts
 }
+ENDIF
 
 ; fetch a byte from the current decode buffer at the current read ptr offset
 ; returns byte in A, clobbers Y
@@ -313,7 +326,201 @@ ENDIF
     jmp try_literal
 }
 
-.lz_end
+
+
+
+IF USE_HUFFMAN
+
+;-------------------------------
+; huffman decoder
+;-------------------------------
+
+
+
+; 6502 workspace - assuming max 16-bit codes
+; init
+; (2) table_bitlengths - only referenced once, can perhaps be self modified
+; (2) table_symbols - only referenced once, can perhaps be self modified, implied +16 from table_bitlengths
+; per stream
+; (2) stream read ptr (can replace the one used by lz4, so no extra)
+; (1) bitbuffer
+; (1) bitsleft
+; per symbol fetch
+; (2) code
+; (2) firstCodeWithNumBits
+; (1) startIndexForCurrentNumBits
+; (1) code_size
+; (1) numCodes
+; (1) indexForCurrentNumBits
+; Note that table does not necessarily require MAX_SYMBOLS bytes now, will contain 16 entries plus N symbols. If few symbols occur.
+; Could be an argument for separate tables per stream if compression ratio beats table overhead.
+; we cant interleave the lz4 data because variable bytes needed per register stream per frame
+; therefore we have to maintain 8 huffman contexts also.
+
+; we use the LZ read ptr
+huff_readptr    = zp_stream_src ; HUFF_ZP + 2 ; 2
+
+
+HUFF_ZP = &70
+
+
+huff_code       = HUFF_ZP + 4 ; 2
+huff_firstcode  = HUFF_ZP + 6 ; 2
+huff_startindex = HUFF_ZP + 8 ; 1
+huff_codesize   = HUFF_ZP + 9 ; 1
+huff_index      = HUFF_ZP + 10 ; 2
+huff_numcodes   = HUFF_ZP + 12 ; 1
+huff_temp       = HUFF_ZP + 13 ; 2
+
+.length_table
+.symbol_table
+
+
+;.huff_init
+;{
+;    lda # 0
+;    sta huff_bitbuffer  ;bitbuffer = 0
+;    sta huff_bitsleft   ;numbitsbuffered = 0
+;
+;    lda #0
+;    sta huff_readptr + 0
+;    sta huff_readptr + 1
+;    rts
+;}
+
+; fetch a byte from the currently selected huffman compressed register data stream
+; returns byte in A, clobbers Y - same as lz_fetch_byte
+.lz_fetch_byte
+.huff_fetch_byte
+{
+    sta huff_code + 0       ;code = 0                            # word
+    sta huff_code + 1
+    sta huff_codesize ;code_size = 0                       # byte
+
+    sta huff_firstcode + 0 ; firstCodeWithNumBits = 0            # word
+    sta huff_firstcode + 1
+
+    sta huff_startindex ; startIndexForCurrentNumBits = 0     # byte
+
+    ;sourceindex = 0
+    ;unpacked = 0
+    ;while unpacked < unpacked_size: # currentbyte < len(data):
+
+}
+.decode_loop
+{
+    ;# keep the bitbuffer going
+    ;if numbitsbuffered == 0:
+    ;    # we're out of data, so any wip codes are invalid due to byte padding.
+    ;    #if currentbyte >= len(data):
+    ;    #    break
+    ;
+    ;   bitbuffer = data[currentbyte]
+    ;   currentbyte += 1
+    ;   numbitsbuffered += 8
+
+
+    lda huff_bitsleft
+    bne got_bits
+    ; fetch more bits
+    ldy #0
+    lda (huff_readptr), y
+    sta huff_bitbuffer
+    lda #8
+    sta huff_bitsleft
+    inc huff_readptr + 0
+    beq got_bits
+    inc huff_readptr + 1
+.got_bits
+
+    ;# get a bit
+    ;bit = (bitbuffer & 128) >> 7
+    ;bitbuffer <<= 1
+    ;numbitsbuffered -= 1
+
+    ;# build code
+    ;code = (code << 1) | bit
+    ;code_size += 1
+
+    ; build code
+    dec huff_bitsleft
+    asl huff_bitbuffer           ; bit7 -> C
+    asl huff_code + 0       ; C -> bit0, bit7 -> C
+    rol huff_code + 1       ; C -> bit8
+    inc huff_codesize
+
+    ;# how many canonical codes have this many bits
+    ;assert code_size <= Huffman.MAX_CODE_BIT_LENGTH
+    ;numCodes = length_table[code_size] # self.table_bitlengths[code_size] # byte
+    ldy huff_codesize
+}
+.LOAD_LENGTH_TABLE
+{
+    lda length_table, Y     ; ** MODIFIED **
+    sta huff_numcodes
+
+    ;# if input code so far is within the range of the first code with the current number of bits, it's a match
+    ;indexForCurrentNumBits = code - firstCodeWithNumBits
+
+    sec
+    lda huff_firstcode + 0
+    sbc huff_code + 0
+    sta huff_index + 0
+    lda huff_firstcode + 1
+    sbc huff_code + 1
+    sta huff_index + 1
+
+    ;if indexForCurrentNumBits < numCodes:
+    
+    ; if hi byte is non zero, is definitely > numcodes
+    ; or numcodes >= index
+    bne nextbit 
+    lda huff_index
+    cmp huff_numcodes
+    bcs nextbit
+    
+    ; code = startIndexForCurrentNumBits + indexForCurrentNumBits
+    lda huff_startindex
+    clc
+    adc huff_index
+    tay
+}
+.LOAD_SYMBOL_TABLE
+{
+    ; symbol = symbol_table[code]
+    lda symbol_table, Y     ; ** MODIFIED **
+    rts
+}
+.nextbit
+{
+    ; otherwise, move to the next bit length
+    ; firstCodeWithNumBits += numCodes
+    lda huff_firstcode + 0
+    clc
+    adc huff_numcodes
+    sta huff_firstcode + 0
+    lda huff_firstcode + 1
+    adc #0
+    sta huff_firstcode + 1
+
+    ; firstCodeWithNumBits <<= 1
+    asl huff_firstcode + 0
+    rol huff_firstcode + 1
+    
+    ; startIndexForCurrentNumBits += numCodes
+    lda huff_startindex
+    clc
+    adc huff_numcodes
+    sta huff_startindex
+    
+    ; keep going until we find a symbol
+    jmp decode_loop
+}
+
+ENDIF ; USE_HUFFMAN
+
+
+.decoder_end
 ;-------------------------------------------
 
 
@@ -322,15 +529,15 @@ ENDIF
 
 ; local vgm workspace
 .vgm_streams ; decoder contexts - 8 bytes per stream, 8 streams (64 bytes)
-    skip  8*8
+    skip  8*lz_zp_size
     ;zp_stream_src   = VGM_ZP + 0    ; stream data ptr LO/HI
     ;zp_literal_cnt  = VGM_ZP + 2    ; literal count LO/HI
     ;zp_match_cnt    = VGM_ZP + 4    ; match count LO/HI
     ;zp_window_src   = VGM_ZP + 6    ; window read ptr - index
     ;zp_window_dst   = VGM_ZP + 7    ; window write ptr - index
 
-.vgm_buffers  equb 0
-.vgm_finished equb 0
+.vgm_buffers  equb 0    ; the HI byte of the address where the buffers are stored
+.vgm_finished equb 0    ; a flag to indicate player has reached the end of the vgm stream
 
 ; A contains data to be written to sound chip
 ; clobbers X
@@ -386,9 +593,36 @@ ENDIF
 	rts
 }
 
+; when mounting a VGM file we use these two variables as temporaries
+zp_block_data = VGM_ZP+0
+zp_block_size = VGM_ZP+2
 
+; on entry zp_block_data points to current block (header)
+; on exit zp_block_data points to next block
+.vgm_next_block
+{
+    ; read 16-bit block size to zp_block_size
+    ; +4 to compensate for block header
+    ldy #0
+    lda (zp_block_data),Y
+    clc
+    adc #4
+    sta zp_block_size+0
+    iny
+    lda (zp_block_data),Y
+    adc #0
+    sta zp_block_size+1
 
-
+    ; move to next block
+    lda zp_block_data+0
+    clc
+    adc zp_block_size+0
+    sta zp_block_data+0
+    lda zp_block_data+1
+    adc zp_block_size+1
+    sta zp_block_data+1
+    rts
+}
 
 
 
@@ -401,8 +635,6 @@ ENDIF
     ;  Magic number[4], Flags[1], MaxBlockSize[1], Header checksum[1]
     ;  Contains 8 blocks
     ; Obviously since this is an 8-bit CPU no files or blocks can be > 64Kb in size
-zp_block_data = VGM_ZP+0
-zp_block_size = VGM_ZP+2
 
     ; Skip frame header, and move to first block
     txa
@@ -413,27 +645,59 @@ zp_block_size = VGM_ZP+2
     adc #0
     sta zp_block_data+1
 
+
+IF USE_HUFFMAN
+    ; first block contains the bitlength and symbol tables
+
+zp_symbol_table_size = zp_stream_src + 0
+zp_length_table_size = zp_stream_src + 1
+
+    ; stash table sizes for later
+    ldy #8
+    lda (zp_block_data),Y   ; symbol table size
+    sta zp_symbol_table_size    
+    iny
+    lda (zp_block_data),Y   ; bitlength table size
+    sta zp_length_table_size    ;; table size + 6 will never be > 256
+
+    ; store the address of the bitlengths table directly in the huff_fetch_byte routine
+    lda zp_block_data + 0
+    clc
+    adc #4+4+1        ; skip lz blocksize, huff block size and symbol count byte
+    sta LOAD_LENGTH_TABLE + 0
+    lda zp_block_data + 0
+    adc #0
+    sta LOAD_LENGTH_TABLE + 1
+
+    ; store the address of the symbols table directly in the huff_fetch_byte routine
+    lda LOAD_LENGTH_TABLE + 0
+    clc
+    adc zp_length_table_size
+    sta LOAD_SYMBOL_TABLE + 0
+    lda LOAD_LENGTH_TABLE + 1
+    adc #0
+    sta LOAD_SYMBOL_TABLE + 1
+
+    ; skip to next block
+    jsr vgm_next_block
+
+
+ENDIF ; USE_HUFFMAN
+
+
     ; read the block headers (size)
     ldx #0
 .block_loop
-
-    ; read 16-bit block size to zp_block_size
-    ldy #0
-    lda (zp_block_data),Y
-    sta zp_block_size+0
-    iny
-    lda (zp_block_data),Y
-    sta zp_block_size+1
-
+ 
     ; get address of block, store in vgm_streams[x*8]
     lda zp_block_data+0
     clc
-    adc #4
-    sta zp_block_data+0
+    adc #4  ; skip block header
+    ;sta zp_block_data+0
     sta vgm_streams + 0, x
     lda zp_block_data+1
     adc #0
-    sta zp_block_data+1
+    ;sta zp_block_data+1
     sta vgm_streams + 1, x
 
     ; init the rest
@@ -444,23 +708,19 @@ zp_block_size = VGM_ZP+2
     sta vgm_streams + 5, x  ; match cnt 
     sta vgm_streams + 6, x  ; window src ptr 
     sta vgm_streams + 7, x  ; window dst ptr 
+IF USE_HUFFMAN
+    sta vgm_streams + 8, x  ; huff bitbuffer
+    sta vgm_streams + 9, x  ; huff bitsleft
+ENDIF
 
-
-    ; move to next block
-    lda zp_block_data+0
-    clc
-    adc zp_block_size+0
-    sta zp_block_data+0
-    lda zp_block_data+1
-    adc zp_block_size+1
-    sta zp_block_data+1
+    jsr vgm_next_block
 
     ; for all 8 blocks
     txa
     clc
-    adc #8
+    adc #lz_zp_size
     tax
-    cpx #8*8
+    cpx #8*lz_zp_size
     bne block_loop
 
     rts
@@ -479,7 +739,7 @@ zp_block_size = VGM_ZP+2
     sta lz_zp, y
     inx
     iny
-    cpy #8
+    cpy #lz_zp_size
     bne loop
     rts
 }
@@ -496,7 +756,7 @@ zp_block_size = VGM_ZP+2
     sta vgm_streams, x
     inx
     iny
-    cpy #8
+    cpy #lz_zp_size
     bne loop
     rts    
 }
@@ -520,19 +780,22 @@ zp_block_size = VGM_ZP+2
     asl a
     asl a
     asl a
+IF USE_HUFFMAN
+    asl a
+ENDIF
     sta temp
     tax
 
     ; since we have 8 separately compressed register streams
     ; we have to load the required decoder context to ZP
-    jsr vgm_load_register_context
+    jsr vgm_load_register_context   ; TODO:inline
 
     ; then fetch a decompressed byte
     jsr lz_decode_byte
     pha
     ldx temp
     ; then we save the decoder context from ZP back to main ram
-    jsr vgm_save_register_context
+    jsr vgm_save_register_context   ; TODO:inline
     pla
     rts
 .temp equb 0
@@ -637,22 +900,27 @@ zp_block_size = VGM_ZP+2
 
 
 .vgm_data
-;INCBIN "data/nd-ui.bin.lz4"
+IF USE_HUFFMAN
+INCBIN "data/outruneu.bin.vgc"
+;INCBIN "data/darkside1.bin.vgc"
 ;INCBIN "data/androids.bin.lz4"
-;INCBIN "data/mongolia.bin.lz4"
-;INCBIN "data/mongolia.bin.vgc"
-INCBIN "data/darkside1.bin.vgc"
+ELSE
 ;INCBIN "data/CPCTL10A.bin.vgc"
+INCBIN "data/mongolia.bin.vgc"
+ENDIF
+;INCBIN "data/nd-ui.bin.lz4"
+;INCBIN "data/mongolia.bin.lz4"
+
 
 PRINT ~vgm_data
 
 
 
-PRINT "   lz code size is", (lz_end-lz_start), "bytes"
-PRINT "  vgm code size is", (vgm_end-vgm_start), "bytes"
-PRINT "vgm buffer size is", (vgm_buffer_end-vgm_buffer_start), "bytes"
+PRINT "    decoder code size is", (decoder_end-decoder_start), "bytes"
+PRINT " vgm player code size is", (vgm_end-vgm_start), "bytes"
+PRINT "      vgm buffer size is", (vgm_buffer_end-vgm_buffer_start), "bytes"
 
-PRINT "total vgm player size is", (vgm_buffer_end-vgm_buffer_start) + (lz_end-lz_start) + (vgm_end-vgm_start), "bytes"
+PRINT "total vgm player size is", (vgm_buffer_end-vgm_buffer_start) + (decoder_end-decoder_start) + (vgm_end-vgm_start), "bytes"
 
 
 .end
