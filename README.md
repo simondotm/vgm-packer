d2ccded590bf4b22e8dd13512617358abe5b2236
simondotm <simondotm@users.noreply.github.com>
2019-02-05 23:55:41 +0000

    demo & readme

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # vgm-packer
-Compression experiments for packing VGM music data 
+Python based compression tool for packing SN76489 based VGM music data into an optimal format for use on 8-bit CPU's.
+
+Full writeup coming soon, but basic tool chain is:
+
+`vgmpacker.py <vgmfile>` to create a `.vgc` file which can be loaded and processed by the code in `vgmdemo.asm` and `lib\vgmplayer.h.asm`. The tool only works with VGM files for the SN76489 PSG.
+
+### Command Line Usage
+
+```
+VgmPacker.py : VGM music compressor for 8-bit CPUs
+Written in 2019 by Simon Morris, https://github.com/simondotm/vgm-packer
+
+usage: vgmpacker.py [-h] [-o <output>] [-b <n>] [-n] [-v] input
+
+positional arguments:
+  input                 VGM source file (must be single SN76489 PSG format)
+                        [input]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o <output>, --output <output>
+                        write VGC file <output> (default is '[input].vgc')
+  -b <n>, --buffer <n>  Set decoder buffer size to <n> bytes, default: 255
+  -n, --nohuffman       Disable huffman compression
+  -v, --verbose         Enable verbose mode
+
+Notes:
+ Buffer size <256 bytes emits 8-bit LZ4 offsets, medium compression, faster decoding, 2Kb workspace
+ Buffer size >255 bytes emits 16-bit LZ4 offsets, higher compression, slower decoding, Size*8 workspace
+ Disabling huffman will result in slightly worse compression, but faster and less variable decoding speed
+
+```
+### 6502 Player Usage
+Import `lib\vgmplayer.h.asm` and `lib\vgmplayer.asm` into your BeebAsm project. Note that you should `INCLUDE "lib/vgmplayer.h.asm"` at the point where you declare your zeropage vars - see `vgmdemo.asm` for an example.
+
+#### `vgmplayer.asm`
+
+You can assemble this module a couple of ways and there are defines in `vgmplayer.h.asm` as follows:
+
+`ENABLE_HUFFMAN`
+* If set to TRUE, the decoder will be able to load `.vgc` files compiled by `vgmpacker.py` with Huffman enabled (this is the default setting)
+* If set to FALSE, the decoder will not include any code to decode huffman `.vgc` files so make sure you compress your `.vgm` files using the `-n` option with `vgmpacker.py`.
+
+Huffman decoding is a fair bit slower, but does save 5-10% in compression ratio, so I recommend only using it for situations where you need maximum compression and CPU runtime & code size is not an constraint.
+
+There are 2 main user routines:
+
+`vgm_init()` - to initialize the player with a VGC data stream
+
+`vgm_update()` - to refresh the player every 50Hz
+
+See `vgmplayer.asm` for more information.
+
+#### Memory Requirements
+The Huffman enabled decoder requires 19 zero page vars.
+
+The non-Huffman enabled decoder requires 8 zero page vars.
+
+Both decoders require a 2Kb page aligned workspace ram buffer which can be passed to the decoder via `vgm_init()`.
+
--- vgmdemo.asm
+++ vgmdemo.asm
@@ -5,22 +5,10 @@
 ;******************************************************************
 
 
-SPEED_TEST = FALSE   ;  *run Main in MODE2 to see blue line where vgmplayer finishes work
-TEST_DATA = FALSE
-
 ; Allocate vars in ZP
 .zp_start
 ORG &70
 GUARD &8f
-
-
-;----------------------------------------------------------------------------------------------------------
-; Common global defines
-;----------------------------------------------------------------------------------------------------------
-INCLUDE "lib/bbc.h.asm"
-INCLUDE "lib/bbc_utils.h.asm"
-
-
 
 ;----------------------------------------------------------------------------------------------------------
 ; Common code headers
@@ -29,6 +17,8 @@ INCLUDE "lib/bbc_utils.h.asm"
 
 INCLUDE "lib/vgmplayer.h.asm"
 
+.fx_temp SKIP 2
+.fx_temp1 SKIP 1
 .zp_end
 
 
@@ -67,38 +57,36 @@ ALIGN 256
 
 .vgm_buffer_end
 
-
 .main
 {
+    lda #22
+    jsr &ffee
+    lda #7
+    jsr &ffee
+
     ; initialize the vgm player with a vgc data stream
     lda #hi(vgm_stream_buffers)
     ldx #lo(vgm_data)
     ldy #hi(vgm_data)
+    sec
     jsr vgm_init
-
-;    lda #22
-;    jsr &ffee
-;    lda #2
-;    jsr &ffee
-
-
-IF TEST_DATA
-    jmp mytest
-ENDIF
 
     ; loop & update
     sei
 .loop
-    ;lda #19:jsr &fff4
 
 ; set to false to playback at full speed for performance testing
 IF TRUE 
+    ; vsync
     lda #2
     .vsync1
     bit &FE4D
     beq vsync1
     sta &FE4D
 ENDIF
+
+
+    ;ldy#10:.loop0 ldx#0:.loop1 nop:nop:dex:bne loop1:dey:bne loop0
 
     lda #&03:sta&fe21
     jsr vgm_update
@@ -112,161 +100,16 @@ ENDIF
 
 
 
-IF TEST_DATA
-.testdata
-INCBIN "data/nd-ui.vgm.3.part"
-
-.hex equs "0123456789ABCDEF"
-.drawnum
-{
-    pha
-    and #&f0
-    lsr a:lsr a:lsr a:lsr a
-    tax
-    lda hex, X
-    jsr &ffee
-    pla
-    and #&0f
-    tax
-    lda hex, X
-    jsr &ffee
-
-    rts
-}
-
-
-
-ALIGN 256
-.mytest
-{
-START = testdata
-;DLEN0 = &3f2
-;DLEN1 = &3d5
-;DLEN2 = &457
-;DLEN3 = &48
-;DLEN4 = &1ea
-;DLEN5 = &18
-;DLEN6 = &5d
-;DLEN7 = &b0
-
-DLEN0=&162
-DLEN1=&192
-DLEN2=&21b
-DLEN3=&2f
-DLEN4=&18a
-DLEN5=&10
-DLEN6=&4f
-DLEN7=&6e
-
-OFFSET0 = START ; + 4 + 7
-OFFSET1 = OFFSET0 + DLEN0 + 4
-OFFSET2 = OFFSET1 + DLEN1 + 4
-OFFSET3 = OFFSET2 + DLEN2 + 4
-OFFSET4 = OFFSET3 + DLEN3 + 4
-OFFSET5 = OFFSET4 + DLEN4 + 4
-OFFSET6 = OFFSET5 + DLEN5 + 4
-OFFSET7 = OFFSET6 + DLEN6 + 4
-
-STREAM = 3
-OFFSET = OFFSET3
-DLEN = DLEN3
-
-
-    lda #STREAM
-   ; set the stream buffer (is fixed)
-    tax
-    clc
-    adc vgm_buffers ; hi byte of where the 2kb vgm stream buffer is located
-    sta zp_buffer+1
-    lda #0
-    sta zp_buffer+0
-    txa
-    
-    ; calculate the stream buffer context
-    asl a
-    asl a
-    asl a
-IF USE_HUFFMAN
-    asl a
-ENDIF
-    sta temp
-    tax
-
-    ; since we have 8 separately compressed register streams
-    ; we have to load the required decoder context to ZP
-    jsr vgm_load_register_context   ; TODO:inline
-
-
-    lda #lo(testdata)
-    sta &90
-    lda #hi(testdata)
-    sta &91
-
-    lda #lo(DLEN)
-    sta &92
-    lda #hi(DLEN)
-    sta &93
-
-
-    .loop
-    ;jsr lz_fetch_byte:sta &94
-    jsr vgm_update_register1:sta &94
-
-
-    ldy #0
-    lda (&90),Y
-    cmp &94
-    beq ok
-
-    lda &91:jsr drawnum
-    lda &90:jsr drawnum
-    lda #32:jsr &ffee
-
-    lda &94:jsr drawnum
-    lda #32:jsr &ffee
-    lda #32:jsr &ffee
-.ok
-
-    inc &90
-    bne skip
-    inc &91
-.skip
-
-    dec &92
-    bne skip2
-    lda &93
-    beq end
-    dec &93
-.skip2
-
-    ;jsr &ffe0
-    jmp loop
-
-.end 
-lda#65:jsr &ffee
-rts
-jsr &ffe0
-.temp equb 0
-}
-ENDIF
-
-
 INCLUDE "lib/vgmplayer.asm"
 
-
-
 .vgm_data
-INCBIN "testvgm/nd-ui.vgc"
-;INCBIN "testvgm/androids.vgc"
-;INCBIN "testvgm/syner5.vgc"
-;INCBIN "testvgm/darkside1.vgc"
+INCBIN "testvgm/ym_009.vgc"
+;INCBIN "testvgm/song_091.vgc"
+;INCBIN "testvgm/axelf.vgc"
 ;INCBIN "testvgm/bbcapple.vgc"
-;INCBIN "testvgm/mongolia.vgc"
-;INCBIN "testvgm/things.vgc"
-;INCBIN "testvgm/lethal7.vgc"
-;INCBIN "testvgm/CPCTL10A.vgc"
-;INCBIN "testvgm/bestpart.vgc"
-;INCBIN "testvgm/tale7.vgc"
+;INCBIN "testvgm/nd-ui.vgc"
+;INCBIN "testvgm/outruneu.vgc"
+
 
 
 PRINT ~vgm_data
